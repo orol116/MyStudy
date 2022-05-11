6e0f04a6ee7c5447cfac20bc89bbff3476f7171d
주빈 <orol116@naver.com>
2022-05-11 09:00:40 +0900

    자바스크립트 개념 및 기본 사용법 학습

---
+++ FRONT/Javascript/js/01_개요.js
@@ -0,0 +1,16 @@
+    // 한 줄 주석
+    /* 범위 주석 */
+
+    // js 파일은 <script> 태그 내부라고 생각하면 된다.
+
+    function btnClick2(){
+        alert("external 버튼이 클릭되었습니다.");
+    }
+
+    function changeColor1(){
+        document.getElementById("box").style.backgroundColor = "red";
+    }
+
+    function changeColor2(){
+        document.getElementById("box").style.backgroundColor = "yellow";
+    }
